22c27c326b87ab163d938ecf1f9520de63b311f5
tom_glb <cj_glb@cj.com>
2018-09-03 21:03:07 +0800

    第一次在eclipse中提交

---
+++ src/main/java/com/sr/git/Test.java
@@ -0,0 +1,11 @@
+package com.sr.git;
+
+/** 
+ * @ClassName: Test 
+ * @Description: TODO
+ * @author: 蔡君Cc_cJ
+ * @date: 2018年9月3日 下午8:08:53  
+ */
+public class Test {
+
+}
---
+++ src/test/java/com/sr/test/GoodTest.java
@@ -0,0 +1,11 @@
+package com.sr.test;
+
+/** 
+ * @ClassName: GoodTest 
+ * @Description: TODO
+ * @author: 蔡君Cc_cJ
+ * @date: 2018年9月3日 下午8:13:24  
+ */
+public class GoodTest {
+
+}
